757b44366618a3d2ca330223e2616aee148a05a5
Speccy-Rom <speccy.rom@gmail.com>
2021-04-26 22:35:26 +0300

    add serializers tag and views tag

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, re_path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import PostViewSet
+from .views import PostViewSet, TagDetailView, TagView
 from app import views
 
 router = DefaultRouter()
@@ -10,6 +10,9 @@ router.register('posts', PostViewSet, basename='posts')
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("tags/", TagView.as_view()),
+    path("tags/<slug:tag_slug>/", TagDetailView.as_view()),
+
     # # The home page
     # path('', views.index, name='home'),
     # # Matches any html file
--- app/views.py
+++ app/views.py
@@ -5,11 +5,15 @@ from django.template import loader
 from django.http import HttpResponse
 from django import template
 from rest_framework import viewsets
-from .serializers import PostSerializer
-from .models import Post
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework import pagination
+from rest_framework import generics
+from .serializers import TagSerializer
+from taggit.models import Tag
+
+from .serializers import PostSerializer
+from .models import Post
 
 
 class PageNumberSetPagination(pagination.PageNumberPagination):
@@ -24,6 +28,23 @@ class PostViewSet(viewsets.ModelViewSet):
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
     pagination_class = PageNumberSetPagination
+
+
+class TagDetailView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    pagination_class = PageNumberSetPagination
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug'].lower()
+        tag = Tag.objects.get(slug=tag_slug)
+        return Post.objects.filter(tags=tag)
+
+
+class TagView(generics.ListAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.AllowAny]
 
 
 @login_required(login_url="/login/")
